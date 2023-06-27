8433f015f61a07219b2745e3cc9c7cd0f05021ec
Karim Bekouchi <karim.bekouchi@outlook.com>
2023-06-27 04:10:12 +0200

    Test Unitaire Junit

---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void main() {
+        String a = "Tot";
+
+        assertEquals("Tot", a);
+
+
+    }
+}
